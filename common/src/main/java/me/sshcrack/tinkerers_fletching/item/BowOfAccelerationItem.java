86d4777092f10fc8f20f1e089982ae1db5b0c053
sshcrack <34072808+sshcrack@users.noreply.github.com>
2024-07-18 23:32:59 +0200

    working on bow of acceleration

--- common/src/main/java/me/sshcrack/tinkerers_fletching/item/BowOfAccelerationItem.java
+++ common/src/main/java/me/sshcrack/tinkerers_fletching/item/BowOfAccelerationItem.java
@@ -1,8 +1,5 @@
 package me.sshcrack.tinkerers_fletching.item;
 
-
-import me.sshcrack.tinkerers_fletching.enchantment.PseudoEnchantmentRegistry;
-import me.sshcrack.tinkerers_fletching.enchantment.PseudoEnchantmentSupport;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.BowItem;
@@ -12,13 +9,12 @@ import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
-public class BowOfAccelerationItem extends BowItem implements FletchingItem, PseudoEnchantmentSupport {
+public class BowOfAccelerationItem extends BowItem implements FletchingItem {
     public static float VELOCITY_MULTIPLIER = 1.2f;
     public static double MAX_DAMAGE_MULTIPLIER = 1.2d;
 
     public BowOfAccelerationItem() {
         super(new Item.Settings().maxDamage((int) (384d * MAX_DAMAGE_MULTIPLIER)));
-        PseudoEnchantmentRegistry.registerPseudoEnchantment(this);
     }
 
     @Override
@@ -44,10 +40,5 @@ public class BowOfAccelerationItem extends BowItem implements FletchingItem, Pse
     @Override
     public int getRange() {
         return (int) (super.getRange() * VELOCITY_MULTIPLIER);
-    }
-
-    @Override
-    public Item getPseudoEnchantmentItem() {
-        return Items.BOW;
     }
 }
